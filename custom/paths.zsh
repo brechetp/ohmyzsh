c2d83a2648bb1f30efa7a87674c4469d005f0517
Pierre Bréchet <brechet@mis.mpg.de>
2021-06-23 13:41:08 +0200

    new paths

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -1,11 +1,13 @@
 # usr configuration for the PATH variable
 
-PATH="/usr/local/texlive/2019/bin/x86_64-darwin/:$PATH" # latex
+#PATH="/usr/local/texlive/2020/bin/x86_64-darwin/:$PATH" # latex
 #
 # GNU sed
 PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
 PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+PATH="/usr/local/lib/ruby/gems/2.7.0/bin:$PATH"
 
 #echo $PATH
 
